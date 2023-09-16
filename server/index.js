3dde30f97205b9c3b2fea0cc524a92b04e3fd267
R-lite <muminraj15@gmail.com>
2023-09-16 11:44:31 +0100

    vercel configurations

--- server/index.js
+++ server/index.js
@@ -9,15 +9,14 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import userRoutes from './routes/users.js';
-import postRoutes from './routes/posts.js';
-import { createPost } from './controllers/posts.js';
-import { register } from './controllers/auth.js'
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { createPost } from "./controllers/posts.js";
+import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
-
 
 /* CONFIG */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,37 +33,40 @@ app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FS */
-const storage   = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, "public/assets");
-    },
-    filename: (req, file, cb)=>{
-        cb(null, file.originalname);
-    }
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 const upload = multer({ storage });
 
 /* ROUTES WITH FILES UPLOAD */
-app.post("/auth/register", upload.single("picture"), register); 
+app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
-app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
-app.use('/posts', postRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SERVER */
 const PORT = process.env.PORT || 5001;
-mongoose.set('strictQuery', true);
-mongoose.connect('mongodb+srv://raj:78945maxMAX.@primary-db.ud2ezij.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     app.listen(PORT, () => console.log(`Sever port: ${PORT}`));
-    
+
     /* POPULATE DB WIRH DUMMY USERS ONCE FOR TESTING */
     // User.insertMany(users);
-    // Post.insertMany(posts); 
-}
-).catch((err) => console.log(`${err} did not connect`))
+    // Post.insertMany(posts);
+  })
+  .catch((err) => console.log(`${err} did not connect`));
+
+module.exports = app;
